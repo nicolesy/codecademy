15a5992750d0feeba0587c496b7e991ab9e14666
Nicole Young <nicolesy@Nicoles-MacBook-Pro.local>
2019-08-25 14:31:48 -0700

    Testing diff format

--- projects/codecademy_project_game_of_chance.py
+++ projects/codecademy_project_game_of_chance.py
@@ -1,3 +1,35 @@
 # codecademy_project_game_of_chance.py
 
-d
+import random
+
+#Write your game of chance functions here
+
+
+def winner(call, computer):
+    if call == "heads":
+        call = 1
+    elif call == "tails":
+        call = 2
+    if call == computer:
+        result = "You win!"
+    elif call != computer:
+        result = "You lose."
+    return result
+
+#Call your game of chance functions here
+
+money = 100
+
+while money > 0:
+    user_call = input("Choose one — heads or tails: ").lower
+    user_bet = input("How much money would you like to bet? ")
+    user_bet = int(user_bet)
+    computer_call = random.randint(1,2)
+    print(f"The computer picked {computer_call}")
+    if winner(user_call, computer_call) == "You win!":
+        money += user_bet
+    else:
+        money -= user_bet
+    print(f"{winner(user_call, computer_call)} Your balance is now {money}")
+else:
+    print("You are out of money.")
